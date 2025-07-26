1a5779377ddc250a6d7dfc5c71209079e7892fa1
Youssef Shafek <youssefmsa616@gmail.com>
2025-07-26 20:43:43 +0300

    enable email change

--- src/Components/Pages/UserSetting.jsx
+++ src/Components/Pages/UserSetting.jsx
@@ -210,7 +210,6 @@ export default function UserSetting() {
                                             }
                                         })}
                                         className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                        disabled={true}
                                     />
                                 ) : (
                                     <p className="px-3 py-2 bg-gray-50 rounded-md">{currentUser?.data?.data?.user?.email}</p>
